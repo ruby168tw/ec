d2a1f1edde7938553f0c7497d9c37b138caf418f
Ruby Huang <ruby168tw@gmail.com>
2018-04-29 01:58:23 +0800

    name added

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,14 @@ module Ec
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    dormain: 'gmail.com',
+    user_name: 'postmaster@sandboxec53c097d53a4b90b5a41188660a5808.mailgun.org',
+    password: '6e4c0abbc132002bc5545493238a0ae4-bdd08c82-505f1415',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'quiet-taiga-61532.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
