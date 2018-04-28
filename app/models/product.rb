d40aaf49ab870402b4fea19e96c3591701f520d6
Ruby Huang <ruby168tw@gmail.com>
2018-04-29 00:04:21 +0800

    turn off product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,24 +1,9 @@
 class Product < ApplicationRecord
-	before_action :set_product, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_member!, except: [:index, :show]
+	#before_action :set_product, only: [:show, :edit, :update, :destroy]
+	#before_action :authenticate_member!, except: [:index, :show]
 end
 
-def index
-	@product = Product.paginate(:page => params[:page], per_page: 5)
-end
-
-def show
-end
-
-def new
-end
-
-def edit
-end
-
-def create
-end
-
-def update
-end
+#def index
+	#@product = Product.paginate(:page => params[:page], per_page: 5)
+#end
 
