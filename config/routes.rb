c87e680d3def9144fb4f35dfdafee575715f87a3
Ruby Huang <ruby168tw@gmail.com>
2018-04-28 08:56:39 +0800

    change routes back to root home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get '/', :to => 'home#index'
+   root "home#index"
    devise_for :users, controllers: { sessions: 'users/sessions' }
    devise_for :members, controllers: { sessions: 'members/sessions' }
    
