877fd1bf7c9896b7dfbb7152b485ff1611a15408
Ruby Huang <ruby168tw@gmail.com>
2019-05-06 21:23:11 +0800

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
-  get '/ec', :to => 'home#index'
+  #get '/ec', :to => 'home#index'
   root "home#index"
 
-   resources :user, path: "/ec/user"
-   resources :member, path: "/ec/member"
-   resources :orders, path: "/ec/orders"
-   resources :replenishes, path: "/ec/replenishes"
-   resources :product, path: "/ec/product"
-   resources :categories, path: "/ec/categories"
-   resources :cart, path: "/ec/cart"
-   resources :users, path: "/ec/users"
-   resources :members, path: "/ec/members"
-   resources :home, path: "/ec/home"
+  #  resources :user
+  #  resources :member
+  #  resources :orders
+  #  resources :replenishes
+  #  resources :product
+  #  resources :categories
+  #  resources :cart
+  #  resources :users
+  #  resources :members
+  #  resources :home
    
    devise_for :users, controllers: { sessions: 'users/sessions' }
    devise_for :members, controllers: { sessions: 'members/sessions' }
-   #resources :user, :member, :orders, :replenishes, :product
+   resources :user, :member, :orders, :replenishes, :product
 
 
    resources :categories do
